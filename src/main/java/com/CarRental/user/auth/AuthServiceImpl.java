cf16c7fbb5a824473a432c826aa10f3b7b14fbad
yoginoit <ylakhani39@gmail.com>
2024-10-12 14:03:01 -0500

    Added new components for Angular and authentication, and modified user-related files

---
+++ src/main/java/com/CarRental/user/auth/AuthServiceImpl.java
@@ -0,0 +1,30 @@
+package com.CarRental.user.auth;
+
+import com.CarRental.user.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements  AuthService{
+
+    private  final UserRepository userRepository;
+
+    @Override
+    public UserDto createCustomer(SignupRequest signupRequest) {
+        User user = new User();
+        user.setName(signupRequest.getName());
+        user.setEmail(signupRequest.getEmail());
+        user.setPassword(signupRequest.getPassword());
+        user.setUserRole(UserRole.CUSTOMER);
+        User createedUser = userRepository.save(user);
+        UserDto userDto =new UserDto();
+        userDto.setId(createedUser.getId());
+        return userDto;
+    }
+
+    @Override
+    public boolean hasCustomerWithEmail(String email) {
+        return userRepository.findFirstByEmail(email);
+    }
+}
